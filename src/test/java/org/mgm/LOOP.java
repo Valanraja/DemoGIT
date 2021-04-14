a8189683feed1e480bd57db86cd075bf5bf115d9
Valanraja <c.valanraja22@gmail.com>
2021-04-14 20:29:45 +0530

    Task done

---
+++ src/test/java/org/mgm/LOOP.java
@@ -0,0 +1,41 @@
+package org.mgm;
+import java.util.Scanner;
+public class LOOP {
+	
+
+
+		
+	public static void main(String[] args) {
+		
+		int i=5 ;
+
+		switch (i) 
+	
+		{
+		case 1:
+			System.out.println("My number is " + i);
+			break;
+
+			case 2:
+				System.out.println("My number is " + i);
+				break;
+			case 3:
+				System.out.println("My number is " + i);
+				break;
+			case 4:
+				System.out.println("My number is " + i);
+				break;
+			case 5:
+				System.out.println("My number is " + i);
+				break;
+			
+			default:
+		System.out.println("No case");
+		}
+		
+		
+	}
+	
+
+
+}
